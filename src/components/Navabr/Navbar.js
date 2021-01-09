ff58ca8fe719da94d2da63b2f0285b9b85e4a398
Muniba Waqar <muniba.wq@gmail.com>
2021-01-09 13:33:56 -0700

    Create components in Navbar.js

--- src/components/Navabr/Navbar.js
+++ src/components/Navabr/Navbar.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
 };
 
