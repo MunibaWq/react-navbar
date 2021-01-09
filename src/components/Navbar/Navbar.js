6b511cee744be8f925a7ac6beb02af32ac90305e
Muniba Waqar <muniba.wq@gmail.com>
2021-01-09 13:46:48 -0700

    styling continued

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { Nav, NavLink, Bars, NavMenu, NavBtn } from "./NavbarElements";
 const Navbar = () => {
   return (
     <>
